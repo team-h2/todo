40fef1a5024c9ebe07100136b6333ea30f78cf02
mahnsHarbor <mahns1201@gmail.com>
2021-10-18 13:24:21 +0900

    [add] client basic setting

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config({path: "./config.env"});
+require("dotenv").config({ path: "./config.env" });
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,9 +12,9 @@ app.use(require("./routes/record"));
 const dbo = require("./db/conn");
 
 app.listen(port, () => {
-    dbo.connectToServer(function(err){
-        if(err) console.error(err);
-    });
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
 
-    console.log(`Server is running on prot: ${port}`);
-})
+  console.log(`Server is running on prot: http://localhost:${port}`);
+});
