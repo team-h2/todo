079333ee0d887fd13c9364ebd90f36b3d953e2e3
mahnsHarbor <mahns1201@gmail.com>
2021-10-20 13:06:16 +0900

    [add] home

---
+++ client/src/views/home.jsx
@@ -0,0 +1,27 @@
+const element = (
+  <div>
+    <h3>hello, world!</h3>
+  </div>
+);
+
+export default function Home() {
+  return element;
+}
+// import React, { Component } from "react";
+
+// export default class Home extends Component {
+//   // This is the constructor that shall store our data retrieved from the database
+//   constructor(props) {
+//     super(props);
+//     this.state = { text: "Hello, World!" };
+//   }
+
+//   // This following section will display the table with the records of individuals.
+//   render() {
+//     return (
+//       <div>
+//         <h3>{this.state.text}</h3>
+//       </div>
+//     );
+//   }
+// }
